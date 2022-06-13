d355fd2d58e4ef533c6e41acf845789d50875790
oceansalle <oceanesalleiae@gmail.com>
2022-06-13 15:00:42 +0200

    initialisation

---
+++ fitnessapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+    <Box width="400px">
+     Navbar
+     <Routes>
+       <Route/>
+     </Routes>
+    </Box>
+  );
+}
+
+export default App;
