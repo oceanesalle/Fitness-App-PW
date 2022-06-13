fae1ee7bed29c423c8f81371deca5abd3196c222
oceansalle <oceanesalleiae@gmail.com>
2022-06-13 15:10:19 +0200

    Router

--- fitnessapp/src/App.js
+++ fitnessapp/src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <Box width="400px">
-     Navbar
+     <Navbar/>
      <Routes>
-       <Route/>
+       <Route path="/" element={<Home/>} />
+       <Route path="/exercise/:id" element={<ExerciseDetail/>} />
      </Routes>
+     <Footer/>
     </Box>
   );
 }
