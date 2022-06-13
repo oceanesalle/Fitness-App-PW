b09141337aa7ce088bfec1b2d653092f7c74991e
oceansalle <oceanesalleiae@gmail.com>
2022-06-13 17:21:12 +0200

    home fetchdata

--- fitnessapp/src/pages/Home.js
+++ fitnessapp/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box } from '@mui/material';
 
 import Exercises from '../components/Exercises';
@@ -6,11 +6,14 @@ import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart}/>
     </Box>
   )
 }
