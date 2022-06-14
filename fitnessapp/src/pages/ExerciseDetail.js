6c98ad02f98b6c20004026795860d73f59067a31
oceansalle <oceanesalleiae@gmail.com>
2022-06-14 11:13:07 +0200

    solving error

--- fitnessapp/src/pages/ExerciseDetail.js
+++ fitnessapp/src/pages/ExerciseDetail.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
-import Detail from '../components/Detail';
-import ExerciseVideos from '../components/ExerciseVideos';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/FetchData';
+import Details from '../components/Details';
+import ExercisesVideos from '../components/ExercisesVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
@@ -41,8 +41,8 @@ const ExerciseDetail = () => {
 
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
-      <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <Details exerciseDetail={exerciseDetail} />
+      <ExercisesVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
   );
